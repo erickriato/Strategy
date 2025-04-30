178ec2f44f9da47f63f06257a9b3792b494e0b7b
erick <erickriatopinheiro@hotmail.com>
2025-04-29 23:25:14 -0300

    Implementando o Design Pattern Strategy

---
+++ src/Aritmetica.java
@@ -0,0 +1,15 @@
+public class Aritmetica implements MediaStrategy{
+    @Override
+    public double calcularMedia(double p1, double p2) {
+        return (p1 + p2) / 2;
+    }
+
+    @Override
+    public String verificarSituacao(double media) {
+        if (media >= 5){
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+}
---
+++ src/Geometrica.java
@@ -0,0 +1,15 @@
+public class Geometrica implements MediaStrategy{
+    @Override
+    public double calcularMedia(double p1, double p2) {
+        return Math.sqrt(p1 * p2);
+    }
+
+    @Override
+    public String verificarSituacao(double media) {
+        if(media >= 7){
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+}
